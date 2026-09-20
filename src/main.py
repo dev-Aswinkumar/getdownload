b1c4b12d6b898949acb0a9c0665583461bef2e9a
Aswinkumar <aswinkumar122008@gmail.com>
2026-09-20 13:46:49 +0530

    added aiotorrent to download torrent

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 from pathlib import Path
-def get_Input():
+import asyncio
+from aiotorrent import Torrent
+
+def get_input():
   filepath=Path(input("Enter the filepath: ").strip())
   destpath=Path(input("Enter the destination ").strip())
   if not filepath.is_file():
-    print("The file doesn't exist")
+    print("The file or directory doesn't exist")
     return None,None
   if not destpath.is_dir():
-    print("The directory doesn't exist")
+    print("The file or directory doesn't exist")
     return None,None
   return filepath,destpath
+async def download_torrent(torrent_path, download_path):
+  torrent = Torrent(str(torrent_path))
+  print("\nReading torrent...")
+  await torrent.init()
+  print(f"Torrent: {torrent.name}")
+  for file in torrent.files:
+    print(f"Downloading: {file.path}")
+    await torrent.download(file)
+  print("\nDownload completed!")
+
 def main():
   print("Welcome to getDownlaoded")
   print("========================")
-  filepath,dirpath=get_Input()
-  if filepath==None:
+  filepath,dirpath=get_input()
+  if filepath is None:
     return
- 
+  asyncio.run(
+    download_torrent(torrent_path, download_path)
+  )
 if __name__=="__main__":
   main()

b6598b4a5ad04aa6eec1ee2d8b54e490f831f0d1
Aswinkumar <aswinkumar122008@gmail.com>
2026-09-20 13:05:21 +0530

    getting path input from user

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from pathlib import Path
+def get_Input():
+  filepath=Path(input("Enter the filepath: ").strip())
+  destpath=Path(input("Enter the destination ").strip())
+  if not filepath.is_file():
+    print("The file doesn't exist")
+    return None,None
+  if not destpath.is_dir():
+    print("The directory doesn't exist")
+    return None,None
+  return filepath,destpath
+def main():
+  print("Welcome to getDownlaoded")
+  print("========================")
+  filepath,dirpath=get_Input()
+  if filepath==None:
+    return
+ 
+if __name__=="__main__":
+  main()
